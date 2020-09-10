ac2fa47690b85b2469c0b63d8a1afefd4f38aa83
Dmitriy An <pancake0890@mail.ru>
2020-09-10 19:49:45 +0500

    feat: add typical-arrays-problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array == undefined || array.length == 0) return 0;
+
+  let sorted = array.sort((a, b) => a - b);
+
+  return sorted[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array == undefined || array.length == 0) return 0;
+
+  let sorted = array.sort((a, b) => a - b);
+
+  return sorted[sorted.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array == undefined || array.length == 0) return 0;
+
+  return array.reduce((acc, cur) => acc + cur, 0) / array.length;
 }
